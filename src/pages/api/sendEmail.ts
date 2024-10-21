971acaa9525a616d5d5afdcdbc65525e03e06655
colinamicon <amiconc@oregonstate.edu>
2024-10-21 02:06:37 -0400

    fix tests and build

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,34 +1,33 @@
 import { Resend } from 'resend';
-import type { APIRoute } from "astro";
-
+import type { APIRoute } from 'astro';
 
 const resendApiKey = import.meta.env.PUBLIC_RESEND_API_KEY;
 const resend = new Resend(resendApiKey);
 
 export const POST: APIRoute = async ({ request }) => {
-    const data = await request.formData()
-    const name = data.get('name') as string;
-    const email = data.get('email') as string;
-    const message = data.get('message') as string;
-    // Basic validation
-    if (!name || !email || !message) {
-        return new Response('All fields are required', { status: 400 });
-    }
+  const data = await request.formData();
+  const name = data.get('name') as string;
+  const email = data.get('email') as string;
+  const message = data.get('message') as string;
+  // Basic validation
+  if (!name || !email || !message) {
+    return new Response('All fields are required', { status: 400 });
+  }
 
-    try {
-        await resend.emails.send({
-            from: email, // Your verified sender email
-            to: '[info@topstreammedia.com]', // Recipient email
-            subject: `Contact form submission from ${name}`,
-            text: message,
-        });
+  try {
+    await resend.emails.send({
+      from: email, // Your verified sender email
+      to: '[info@topstreammedia.com]', // Recipient email
+      subject: `Contact form submission from ${name}`,
+      text: message,
+    });
 
-        console.log(data)
-        return new Response('Email sent successfully!', { status: 200 });
-    } catch (error) {
-        console.error('Error sending email:', error);
-        return new Response('Error sending email', { status: 500 });
-    }
+    console.log(data);
+    return new Response('Email sent successfully!', { status: 200 });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return new Response('Error sending email', { status: 500 });
+  }
 };
 
 // export async function GET() {

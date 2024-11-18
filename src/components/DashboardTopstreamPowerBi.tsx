6796be268ba2a22c40f442749fb79867387853b6
colinamicon <cmamicon@gmail.com>
2024-11-17 21:47:46 -0500

    tighten up formatting

--- src/components/DashboardTopstreamPowerBi.tsx
+++ src/components/DashboardTopstreamPowerBi.tsx
@@ -1,14 +1,14 @@
 const isAllowedFullscreen = true;
 
-const styles = { margin: 'auto' };
+const styles = { margin: 'auto', marginTop: '3rem' };
 
 export default function DashboardTopstreamPowerBi() {
   return (
     <iframe
       title="Topstream demo"
       style={styles}
-      width="1700px"
-      height="1000px"
+      width="1800px"
+      height="1200px"
       src="https://app.powerbi.com/view?r=eyJrIjoiNDdiZGE0M2EtMDk0OS00MjQxLWI1N2QtYzU1NDM4NDYzMGFhIiwidCI6IjZlNDRlNGNmLWI0NDUtNDhlNC1iODQwLTIyYzUwMTcyYTQ3ZCJ9&pageName=ae18a507f1f63def2ad3"
       allowFullScreen={isAllowedFullscreen}
     ></iframe>
